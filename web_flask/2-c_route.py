d96689ce003125165a751bcb557abfd3af65cbf8
Olamide Joseph Williams <circleofwilliams@gmail.com>
2023-01-14 18:02:13 -0800

    flask api task

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3                                                  
+""" Script of a flask framework project """                                             
+
+from flask import Flask, render_template                                    
+
+app = Flask(__name__)                                       
+
+@app.route("/", strict_slashes=False)                                   
+def home():                                                        
+    """ A function to return a string """                   
+    return "Hello HBNB!"                            
+
+@app.route("/hbnb", strict_slashes=False)                       
+def hbnb():                                         
+    """ A function to return a string """                       
+    return "HBNB"                                           
+
+@app.route('/c/<text>', strict_slashes=False)                       
+def c(text):                                                    
+    """ A function to return C and user input text """              
+    newText = ""                    
+    for i in text:                                                          
+        if i == '_':                                                
+            i = ' '                             
+        newText += i
+    return (f'C {newtext}')
+
+if __name__ == '__main__':                              
+    app.run(host="0.0.0.0", port=5000)
