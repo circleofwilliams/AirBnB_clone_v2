98dfcc7dbb3d6c33f6e85be40db416f4d9ea890e
Olamide Joseph Williams <circleofwilliams@gmail.com>
2023-01-15 19:32:28 -0800

    Airbnb_clone update

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,11 +1,21 @@
--- prepares a MySQL server for the project
--- GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
+--
+-- Creating Database
+--
 
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+--
+-- Creating User
+--
+
 CREATE USER IF NOT EXISTS hbnb_test@localhost IDENTIFIED BY 'hbnb_test_pwd';
-USE hbnb_test_db;
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-USE performance_schema;
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+
+--
+-- Granting Privileges
+--
+
+GRANT ALL ON hbnb_test_db.* TO hbnb_test@localhost;
+GRANT SELECT ON performance_schema.* TO hbnb_test@localhost;
+
 FLUSH PRIVILEGES;
-SET FOREIGN_KEY_CHECKS=1;
+
--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
-""" Script that starts a flask web application """
+""" Script of a flask framework project """
 
-from flask import Flask
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
-def home():
-	""" A function to serve request to the root """
+def hello():
+	""" A function to return a string """
 	return "Hello HBNB!"
 
+
 if __name__ == '__main__':
-	app.run(host="0.0.0.0", port=5000)
+	app.run(host="0.0.0.0", port=5000, debug=True)
+
